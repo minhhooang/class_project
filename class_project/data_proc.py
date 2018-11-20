382be5fde79b8a49b4c47baba8cec596b8cd84d1
Hoang <minhhooang@gmail.com>
2018-11-20 00:36:53 -0500

    completed data analysis

--- class_project/data_proc.py
+++ class_project/data_proc.py
@@ -19,23 +19,60 @@ PROJECT_DIR = 'class_project'
 DATA_DIR = 'data'
 DEFAULT_DATA_FN = DATA_DIR + '\experimentalData.csv'
 
+MODULUS_0_AVG = 7539.0
+MODULUS_0_STDDEV = 2.2
+
 
 def warning(*objs):  # =====================================================================================
     """Writes a message to stderr."""
     print("WARNING: ", *objs, file=sys.stderr)
 
 
-def data_analysis(df):  # ==================================================================================
+def data_analysis(df, alignment, archi, value_type):  # =================================================================
     """
-        ...
+        - Filtering
+        - Convert to Numpy Array
+        - Convert std. dev. from % to real values
     """
 
-    a = df.loc[df['Defect_alignment'] == 'A']
-    b = a.loc[a['Defect_architecture'] == '-']
+    # FILTERING -----------
+    a = df.loc[df['Defect_alignment'] == alignment]
+    b = a.loc[a['Defect_architecture'] == archi]
     b = b.drop(columns=["Defect_alignment", "Defect_architecture"])
     print(b)
 
-    pass
+    # CONVERTING ----------
+    c = b.values
+    print(c)
+    baseline = [0, MODULUS_0_AVG, MODULUS_0_STDDEV]
+    c = np.vstack([baseline, c])
+    c[:, 2] = np.multiply(c[:, 1], c[:, 2])/100
+
+    if value_type:
+        ylimit = 1.3
+        ylabel_text = value_type + ' modulus (-)'
+        # Normalize
+        c[:, 1] = c[:, 1] / MODULUS_0_AVG
+        c[:, 2] = c[:, 2] / MODULUS_0_AVG
+    else:
+        ylimit = 8500
+        ylabel_text = ' modulus (ksi)'
+
+    print(c)
+
+    # PLOTTING ------------
+    # plt.plot(b[:, 0], b[:, 1], 'bs') # cannot plot data frame ...
+    plt.plot([0, 5.5], [c[0, 1], c[0, 1]], 'b--')
+    plt.plot(c[:, 0], c[:, 1], 'r.')
+    plt.errorbar(c[:, 0], c[:, 1], yerr=c[:, 2], fmt='o', capsize=5)
+    plt.title('Modulus of Defect Specimens (' + alignment + archi + ')')
+    plt.xlabel('defect size (in)')
+    plt.ylabel(ylabel_text)
+    plt.axis([0, 0.55, 0, ylimit])
+    plt.grid(True)
+    # plt.show()
+    plt.savefig("resultPlot_" + alignment + archi + ".png")
+    plt.clf()
 
 
 def parse_cmdline(argv):  # ================================================================================
@@ -52,8 +89,8 @@ def parse_cmdline(argv):  # ====================================================
     parser.add_argument("-c", "--csv_data_file", help="Directory and name of the csv file with data to analyzed.",
                         default=DEFAULT_DATA_FN)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-r", "--real_values", help="Plotting real values", action="store_true")
-    group.add_argument("-n", "--normalized_values", help="Plotting normalized values", action="store_true")
+    group.add_argument("-r", "--real_values", help="Plotting real values", action="store_true", default=True)
+    group.add_argument("-n", "--normalized_values", help="Plotting normalized values", action="store_true", default=False)
 
     args = None
     try:
@@ -72,17 +109,34 @@ def parse_cmdline(argv):  # ====================================================
     return args, 0
 
 
+def plot_data(data_array):  # =========================================================================================
+    """
+        Plotting error bars
+        https://matplotlib.org/1.2.1/examples/pylab_examples/errorbar_demo.html
+    """
+    pass
+
+
 def main(argv=None):  # =====================================================================================
     args, ret = parse_cmdline(argv)
     if ret != 0:
         return ret
-    print(args.csv_data)
-    data = args.csv_data
+
+    value_type = ''
+    if args.normalized_values:
+        value_type = 'Normalized'
+    print(value_type)
+
+    # print(args.csv_data)
+    # data = args.csv_data
     # print(data.index)
-    print(data.columns)
+    # print(data.columns)
 
     # print(data.ix[:, 'Defect_alignment'])
-    data_analysis(args.csv_data)
+    data_analysis(args.csv_data, 'A', '-', value_type)
+    data_analysis(args.csv_data, 'A', '+', value_type)
+    data_analysis(args.csv_data, 'S', '-', value_type)
+    data_analysis(args.csv_data, 'S', '+', value_type)
 
     return 0  # success
 
